fd5a5193394ad4dd2994bfa9f11eb3c2e7f69bb0
Vlad Kostikov <vladkostikovru@gmail.com>
2023-01-31 17:39:37 +0300

    Добавил для станции метод итератор по поездам с блоком

---
+++ lesson_7/main.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+# Изначальный код взят из 6 урока
+# 1. У класса RailwayStation написать метод, который принимает блок
+# и выполняет действия из блока над каждым поездом, находящимся
+# в данный момент на станции.
+
+require_relative 'modules/interface'
+
+include Interface
+
+run_interface
---
+++ lesson_7/train/train.rb
@@ -0,0 +1,143 @@
+# frozen_string_literal: true
+
+require_relative '../modules/manufacturer'
+require_relative '../modules/instances'
+
+# Поезд
+class Train
+  include Manufacturer
+  include Instances
+
+  # Формат номера: 3 буквы или цифры, необязательный дефис, 2 буквы или цифры
+  NUMBER_FORMAT = /^[А-ЯA-Z\d]{3}-?[А-ЯA-Z\d]{2}$/i.freeze
+
+  attr_accessor :station, :previous_station, :route
+  attr_reader :carriages, :speed, :number
+
+  def initialize(number)
+    @number = number.to_s
+    validate!
+    @carriages = []
+    @speed = 0
+    register_instance
+  end
+
+  def permitted_type_carriage
+    Carriage
+  end
+
+  def attachable_carriage?(carriage)
+    carriage.is_a?(permitted_type_carriage)
+  end
+
+  def pick_up_speed
+    @speed = default_speed
+  end
+
+  def attach_carriage(carriage)
+    raise impossible_attach_detach unless stopped?
+    raise failed_attach unless attachable_carriage?
+
+    carriages << carriage
+    print_successful_attach
+  end
+
+  def detach_carriage
+    raise impossible_attach_detach unless stopped?
+
+    if carriages.pop
+      print_successful_detach
+    else
+      print_failed_detach
+    end
+  end
+
+  def next_station
+    return print_route_not_exist unless route.is_a?(Route)
+
+    # Меняем маршрут в обратную сторону, если сейчас поезд на конечной
+    route.stations.reverse if final_stop?
+
+    current_station_index = route.stations.index(station)
+    @next_station = route.stations[current_station_index + 1]
+  end
+
+  def go_next
+    station.send_train(self)
+    stop
+  end
+
+  def stop
+    @speed = 0
+  end
+
+  def stopped?
+    speed.zero?
+  end
+
+  def final_stop?
+    station == route.stations.last
+  end
+
+  def change_number(number)
+    @number = number.to_s
+  end
+
+  def self.find(number)
+    all.find { |train| train.number == number }
+  end
+
+  def valid?
+    validate!
+  rescue StandardError
+    false
+  end
+
+  protected
+
+  attr_writer :number
+
+  # Метод вынесен в protected, потому что используется только
+  # другими методами, является вспомогательным и наследуется дочерним классам.
+  def impossible_attach_detach
+    'Невозможно прицеплять и отцеплять вагоны, во время движения.'
+  end
+
+  def validate!
+    raise 'У поезда должен быть номер' if number.nil? || number.size.zero?
+    raise 'Номер должен быть длиной 5 или 6 символов' if number.size != 5 && number.size != 6
+    raise 'Неправильный формат номера' if number !~ NUMBER_FORMAT
+    raise 'Поезд с таким номером уже существует' if
+      Train.find(number) || CargoTrain.find(number) || PassengerTrain.find(number)
+
+    true
+  end
+
+  def default_speed
+    80
+  end
+
+  def total_carriages_attached
+    "Всего вагонов прицеплено: #{carriages.size}."
+  end
+
+  def print_successful_attach
+    puts "Прицепили 1 вагон. #{total_carriages_attached}"
+  end
+
+  def failed_attach
+    "Не получилось прицепить вагон. #{total_carriages_attached}"
+  end
+
+  def print_successful_detach
+    puts "Отцепили 1 вагон. #{total_carriages_attached}"
+  end
+
+  def print_failed_detach
+    puts "Не получилось отцепить вагон. #{total_carriages_attached}"
+  end
+
+  def print_route_not_exist
+    puts 'Отсутствует маршрут'
+  end
+end
