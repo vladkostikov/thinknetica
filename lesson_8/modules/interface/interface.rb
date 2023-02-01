6acf3f0ef54b7bfe6ffb210c412a052416eeec7f
Vlad Kostikov <vladkostikovru@gmail.com>
2023-02-01 16:14:28 +0300

    Перенёс действия с поездами в отдельный модуль

--- lesson_8/modules/interface/interface.rb
+++ lesson_8/modules/interface/interface.rb
@@ -1,15 +1,15 @@
 # frozen_string_literal: true
 
 require_relative 'station_actions'
+require_relative 'train_actions'
 require_relative '../../route'
-require_relative '../../train/passenger_train'
-require_relative '../../train/cargo_train'
 require_relative '../../carriage/passenger_carriage'
 require_relative '../../carriage/cargo_carriage'
 
 # Текстовый интерфейс программы
 module Interface
   include StationActions
+  include TrainActions
 
   def operations
     {
@@ -20,73 +20,6 @@ module Interface
       move_train: '5. Поместить поезд на станцию',
       print_stations_with_trains: '6. Посмотреть список станций и список поездов на станции'
     }
-  end
-
-  def types_of_trains
-    {
-      PassengerTrain => 'Пассажирский',
-      CargoTrain => 'Грузовой'
-    }
-  end
-
-  def create_train
-    type_of_train = ask_type_of_train
-
-    begin
-      number = ask_number_of_train
-      train = type_of_train.new(number)
-    rescue StandardError => e
-      puts "#{e}\n"
-      retry
-    end
-
-    puts "Поезд #{train} создан."
-  end
-
-  # Возвращает Class выбранного поезда
-  def ask_type_of_train
-    user_choice_number = nil
-    until (1..types_of_trains.size).include?(user_choice_number)
-      puts 'Какой поезд хотите создать?'
-      print_types_of_trains
-      user_choice_number = gets.to_i
-    end
-    index_of_type = user_choice_number - 1
-
-    types_of_trains.to_a[index_of_type][0]
-  end
-
-  def print_types_of_trains
-    types_of_trains.each_with_index do |(_type, type_in_russian), index|
-      puts "#{index + 1}. #{type_in_russian}"
-    end
-  end
-
-  # Возвращает номер поезда
-  def ask_number_of_train
-    puts 'У поезда должен быть номер'
-    puts 'Формат номера: 5 букв или цифр в любом порядке, после 3 символа '\
-       'может быть дефис(необязательно)'
-    print 'Введите номер поезда: '
-    gets.chomp
-  end
-
-  def print_trains
-    raise 'Нет поездов' if trains.empty?
-
-    trains.each_with_index do |train, i|
-      puts "#{i + 1}. #{train}"
-    end
-  end
-
-  # Возвращает поезд
-  def ask_train
-    user_choice_number = nil
-    until (1..trains.size).include?(user_choice_number)
-      print_trains
-      user_choice_number = gets.to_i
-    end
-    trains[user_choice_number - 1]
   end
 
   def attach_carriage
@@ -101,20 +34,6 @@ module Interface
     puts 'От какого поезда хотите отцепить вагон?'
     train = ask_train
     train.detach_carriage
-  end
-
-  def move_train
-    puts 'Какой поезд хотите переместить?'
-    train = ask_train
-    puts 'На какую станцию хотите переместить поезд?'
-    station = ask_station
-
-    station.receive_train(train)
-    puts "Поезд #{train} перемещён на станцию #{station.name}."
-  end
-
-  def trains
-    CargoTrain.all + PassengerTrain.all
   end
 
   def print_menu
